824332133b0f83b2fb26dc62c7182cfddf790173
Bardiya Bakhshandeh <yakari1991@gmail.com>
2019-06-27 00:24:10 +0100

    rankings page mostly finished

---
+++ src/components/PlayersRankings.vue
@@ -0,0 +1,221 @@
+<template>
+  <div>
+    <div class="row justify-center q-gutter-x-sm">
+      <div class="col-grow">
+        <q-table
+          title="Overall Top 20"
+          :data="players.topOverall"
+          :columns="overallColumns"
+          :dense="$q.screen.lt.md"
+          :pagination.sync="pagination"
+          :rows-per-page-options="[5,10,0]"
+          row-key="name"
+          class="bg-secondary"
+        >
+          <q-td slot="body-cell-player" slot-scope="value" :props="value">
+            <router-link :to="'/players/' + value.value.id" class="no-decor" >
+              <div v-if="$q.platform.is.desktop" class="q-mx-aut person-thumbnail no-decor ellipsis">
+                <q-img :src="value.value.picture" :alt="value.value.nickname" class="full-height self-cente" />
+                  <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
+                    {{value.value.firstName}} {{value.value.lastName}}
+                  </q-tooltip>
+              </div>
+              <div v-else >
+                {{value.value.nickname}}
+              </div>
+            </router-link>
+          </q-td>
+        </q-table>
+      </div>
+      <div class="col-auto">
+        <q-table
+          title="U-23 Top 20"
+          :data="players.topU23"
+          :columns="U23Columns"
+          :dense="$q.screen.lt.md"
+          :pagination.sync="pagination"
+          :rows-per-page-options="[5,10,0]"
+          row-key="name"
+          class="bg-secondary"
+        >
+          <q-td slot="body-cell-player" slot-scope="value" :props="value">
+            <router-link :to="'/players/' + value.value.id" class="no-decor" >
+              <div v-if="$q.platform.is.desktop" class="q-mx-aut person-thumbnail no-decor ellipsis">
+                <q-img :src="value.value.picture" :alt="value.value.nickname" class="full-height self-cente" />
+                  <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
+                    {{value.value.firstName}} {{value.value.lastName}}
+                  </q-tooltip>
+              </div>
+              <div v-else >
+                {{value.value.nickname}}
+              </div>
+            </router-link>
+          </q-td>
+        </q-table>
+      </div>
+    </div>
+    <div class="row justify-center q-gutter-md q-pa-md">
+      <div class="col-grow ">
+        <q-table
+          title="Top 20 Goalkeepers"
+          :data="players.topGKs"
+          :columns="GKColumns"
+          :dense="$q.screen.lt.md"
+          :pagination.sync="pagination"
+          :rows-per-page-options="[5,10,0]"
+          row-key="name"
+          class="bg-secondary"
+        >
+          <q-td slot="body-cell-player" slot-scope="value" :props="value">
+            <router-link :to="'/players/' + value.value.id" class="no-decor" >
+              <div v-if="$q.platform.is.desktop" class="q-mx-aut person-thumbnail no-decor ellipsis">
+                <q-img :src="value.value.picture" :alt="value.value.nickname" class="full-height self-cente" />
+                  <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
+                    {{value.value.firstName}} {{value.value.lastName}}
+                  </q-tooltip>
+              </div>
+              <div v-else >
+                {{value.value.nickname}}
+              </div>
+            </router-link>
+          </q-td>
+        </q-table>
+      </div>
+      <div class="col-grow ">
+        <q-table
+          title="Top 20 Defenders"
+          :data="players.topDFs"
+          :columns="DFColumns"
+          :dense="$q.screen.lt.md"
+          :pagination.sync="pagination"
+          :rows-per-page-options="[5,10,0]"
+          row-key="name"
+          class="bg-secondary"
+        >
+          <q-td slot="body-cell-player" slot-scope="value" :props="value">
+            <router-link :to="'/players/' + value.value.id" class="no-decor" >
+              <div v-if="$q.platform.is.desktop" class="q-mx-aut person-thumbnail no-decor ellipsis">
+                <q-img :src="value.value.picture" :alt="value.value.nickname" class="full-height self-cente" />
+                  <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
+                    {{value.value.firstName}} {{value.value.lastName}}
+                  </q-tooltip>
+              </div>
+              <div v-else >
+                {{value.value.nickname}}
+              </div>
+            </router-link>
+          </q-td>
+        </q-table>
+      </div>
+      <div class="col-grow ">
+        <q-table
+          title="Top 20 Midfielders"
+          :data="players.topMDs"
+          :columns="MFColumns"
+          :dense="$q.screen.lt.md"
+          :pagination.sync="pagination"
+          :rows-per-page-options="[5,10,0]"
+          row-key="name"
+          class="bg-secondary"
+        >
+          <q-td slot="body-cell-player" slot-scope="value" :props="value">
+            <router-link :to="'/players/' + value.value.id" class="no-decor" >
+              <div v-if="$q.platform.is.desktop" class="q-mx-aut person-thumbnail no-decor ellipsis">
+                <q-img :src="value.value.picture" :alt="value.value.nickname" class="full-height self-cente" />
+                  <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
+                    {{value.value.firstName}} {{value.value.lastName}}
+                  </q-tooltip>
+              </div>
+              <div v-else >
+                {{value.value.nickname}}
+              </div>
+            </router-link>
+          </q-td>
+        </q-table>
+      </div>
+      <div class="col-auto">
+        <q-table
+          title="Top 20 Attackers"
+          :data="players.topATs"
+          :columns="ATColumns"
+          :dense="$q.screen.lt.md"
+          :pagination.sync="pagination"
+          :rows-per-page-options="[5,10,0]"
+          row-key="name"
+          class="bg-secondary"
+        >
+          <q-td slot="body-cell-player" slot-scope="value" :props="value">
+            <router-link :to="'/players/' + value.value.id" class="no-decor" >
+              <div v-if="$q.platform.is.desktop" class="q-mx-aut person-thumbnail no-decor ellipsis">
+                <q-img :src="value.value.picture" :alt="value.value.nickname" class="full-height self-cente" />
+                  <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
+                    {{value.value.firstName}} {{value.value.lastName}}
+                  </q-tooltip>
+              </div>
+              <div v-else >
+                {{value.value.nickname}}
+              </div>
+            </router-link>
+          </q-td>
+        </q-table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  // name: 'PlayersRankings',
+  data () {
+    return {
+      GKColumns: [
+        { name: 'player', required: true, label: 'Player', align: 'left', field: row => row },
+        { name: 'height', align: 'left', label: 'Height', field: row => row.height, sortable: true },
+        { name: 'score', align: 'left', label: 'Score', field: 'rating', sortable: true },
+      ],
+      DFColumns: [
+        { name: 'player', required: true, label: 'Player', align: 'left', field: row => row },
+        { name: 'Pos', align: 'left', label: 'Pos', field: row => row.specificPosition, sortable: true },
+        { name: 'score', align: 'left', label: 'Score', field: 'rating', sortable: true },
+      ],
+      MFColumns: [
+        { name: 'player', required: true, label: 'Player', align: 'left', field: row => row },
+        { name: 'Pos', align: 'left', label: 'Pos', field: row => row.specificPosition, sortable: true },
+        { name: 'score', align: 'left', label: 'Score', field: 'rating', sortable: true },
+      ],
+      ATColumns: [
+        { name: 'player', required: true, label: 'Player', align: 'left', field: row => row },
+        { name: 'Pos', align: 'left', label: 'Pos', field: row => row.specificPosition, sortable: true },
+        { name: 'score', align: 'left', label: 'Score', field: 'rating', sortable: true },
+      ],
+      U23Columns: [
+        { name: 'player', required: true, label: 'Player', align: 'left', field: row => row },
+        { name: 'Pos', align: 'left', label: 'Pos', field: row => row.specificPosition, sortable: true },
+        { name: 'age', align: 'left', label: 'Age', field: row => date.getDateDiff(date.formatDate(new Date(), 'YYYY-MM-DD'), row.birthday, 'years'), sortable: true },
+        { name: 'score', align: 'left', label: 'Score', field: 'rating', sortable: true },
+      ],
+      overallColumns: [
+        { name: 'player', required: true, label: 'Player', align: 'left', field: row => row },
+        { name: 'nationality', align: 'left', label: 'Nationality', field: 'nationality', sortable: true },
+        { name: 'age', align: 'left', label: 'Age', field: row => date.getDateDiff(date.formatDate(new Date(), 'YYYY-MM-DD'), row.birthday, 'years'), sortable: true },
+        { name: 'pos', align: 'left', label: 'Pos', field: row => row.specificPosition, sortable: true },
+        { name: 'score', align: 'left', label: 'Score', field: 'rating', sortable: true },
+      ],
+      pagination: {
+        rowsPerPage: 10,
+        sortBy: 'score',
+        descending: true,
+      },
+    }
+  },
+
+  props: {
+    players: Object,
+  }
+}
+</script>
+
+<style>
+</style>
--- src/pages/Rankings.vue
+++ src/pages/Rankings.vue
@@ -1,7 +1,8 @@
 <template>
-  <q-page padding>
+  <q-page paddin>
     <q-splitter
       v-model="splitterModel"
+      class="bg-secondary"
     >
 
       <template v-slot:before>
@@ -9,7 +10,7 @@
           v-model="tab"
           vertical
           active-bg-color="secondary"
-          class="bg-primary text-accent"
+          class="bg-primary text-yellow-14"
 
         >
           <q-tab name="teams" icon="mail" label="Teams" />
@@ -46,7 +47,6 @@
                         <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
                           {{value.value.name}}
                         </q-tooltip>
-                        <!-- {{value.value.name}} -->
                     </div>
                   </router-link>
                 </q-td>
@@ -55,9 +55,7 @@
           </q-tab-panel>
 
           <q-tab-panel name="players">
-            <div class="text-h4 q-mb-md">players</div>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <players-rankings :players="players" />
           </q-tab-panel>
 
           <q-tab-panel name="managers">
@@ -95,10 +93,38 @@
             </div>
           </q-tab-panel>
           <q-tab-panel name="fans">
-            <div class="text-h4 q-mb-md">Fans</div>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            <div class="column">
+              <q-table
+                title="Fans Ranking"
+                :data="fans"
+                :columns="fansColumns"
+                :dense="$q.screen.lt.md"
+                :pagination.sync="fansPagination"
+                :rows-per-page-options="[10,20,0]"
+                row-key="name"
+                class="bg-secondary"
+              >
+                <q-td slot="body-cell-fan" slot-scope="value" :props="value">
+                  <div class="row inline person-thumbnail no-decor ellipsis">
+                    <q-img :src=" value.value.picture" :alt="value.value.name" class="full-height self-cente" />
+                  </div>
+                  <router-link :to="'/teams/' + value.value.id" class="no-decor text-bod1 text-capitalize q-ml-sm" >
+                    {{value.value.name}}
+                  </router-link>
+                </q-td>
+
+                <q-td slot="body-cell-team" slot-scope="value" :props="value">
+                  <div class="q-mx-aut  no-decor ellipsis">
+                    <router-link :to="'/teams/' + value.value.id" >
+                      <q-img :src="'statics/' + value.value.logo" :alt="value.value.name" class="full-height team-thumbnail self-cente" />
+                        <q-tooltip :delay="300" :offset="[0, 3]"   transition-show="scale" transition-hide="scale" >
+                          {{value.value.name}}
+                        </q-tooltip>
+                    </router-link>
+                  </div>
+                </q-td>
+              </q-table>
+            </div>
           </q-tab-panel>
         </q-tab-panels>
       </template>
@@ -109,9 +135,15 @@
 
 <script>
 import axios from 'axios'
+import { date } from 'quasar'
+const PlayersRankings = () => import('components/PlayersRankings.vue')
 
 export default {
   name: 'Rankings',
+
+  components: {
+    PlayersRankings
+  },
 
   data () {
     return {
@@ -129,8 +161,6 @@ export default {
           label: 'Team',
           align: 'left',
           field: row => row,
-          // format: val => `${val}`,
-          // sortable: true
         },
         { name: 'totalTrophies', align: 'center', label: 'Major Trophies', field: 'totalTrophies', sortable: true },
         { name: 'popularity', align: 'center', label: 'Popularity', field: 'popularity', sortable: true },
@@ -142,13 +172,9 @@ export default {
       ],
       teams: [],
       managerColumns: [
-        {
-          name: 'manager',
-          required: true,
-          label: 'Manager',
-          align: 'left',
-          field: row => row,
-        },
+        { name: 'manager', required: true, label: 'Manager', align: 'left', field: row => row },
+        { name: 'age', align: 'left', label: 'Age', field: row => date.getDateDiff(date.formatDate(new Date(), 'YYYY-MM-DD'), row.birthday, 'years'), sortable: true },
+        { name: 'nationality', align: 'left', label: 'Nationality', field: row => row.nationality, sortable: true },
         { name: 'team', align: 'left', label: 'Team', field: row => row.team, sortable: true },
         { name: 'score', align: 'left', label: 'Score', field: 'rating', sortable: true },
       ],
@@ -158,6 +184,19 @@ export default {
         descending: true,
       },
       managers: [],
+      players: null,
+      fansColumns: [
+        { name: 'fan', required: true, label: 'Fan', align: 'left', field: row => row },
+        { name: 'nationality', align: 'left', label: 'Nationality', field: row => row.nationality, sortable: true },
+        { name: 'team', align: 'left', label: 'Team', field: row => row.team, sortable: true },
+        { name: 'level', align: 'left', label: 'Level', field: 'fanLevel', format: val => `${val.toFixed(2)}`, sortable: true },
+      ],
+      fansPagination: {
+        rowsPerPage: 10,
+        sortBy: 'level',
+        descending: true,
+      },
+      fans: [],
     }
   },
 
@@ -190,9 +229,31 @@ export default {
             this.error = error
           })
         this.$q.loading.hide()
+      } else if (newVal === 'players' && !this.players) {
+        this.$q.loading.show()
+        axios.get('http://innouts.test/api/rankings/players')
+          .then(response => {
+            this.players = response.data
+          })
+          .catch(error => {
+            this.error = error
+          })
+        this.$q.loading.hide()
+      } else if (newVal === 'fans' && this.fans.length === 0) {
+        this.$q.loading.show()
+        axios.get('http://innouts.test/api/rankings/fans')
+          .then(response => {
+            this.fans = response.data
+          })
+          .catch(error => {
+            this.error = error
+          })
+        this.$q.loading.hide()
       }
     },
+
   }
+
 }
 </script>
 
